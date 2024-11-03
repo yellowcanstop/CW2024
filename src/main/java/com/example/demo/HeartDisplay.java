193622fda27c6ef811e8cedfdc3050d22f10e824
yellowcanstop <yellowcanstop@gmail.com>
2024-11-04 01:06:23 +0800

    Refactor container initialisation into the constructor for HeartDisplay for cleaner code. Method initializeHearts is retained as each heart in the HeartDisplay must be a separate instance.

--- src/main/java/com/example/demo/HeartDisplay.java
+++ src/main/java/com/example/demo/HeartDisplay.java
@@ -9,25 +9,17 @@ public class HeartDisplay {
 	private static final String HEART_IMAGE_NAME = "/com/example/demo/images/heart.png";
 	private static final int HEART_HEIGHT = 50;
 	private static final int INDEX_OF_FIRST_ITEM = 0;
-	private HBox container;
-	private double containerXPosition;
-	private double containerYPosition;
+	private final HBox container;
 	private int numberOfHeartsToDisplay;
 	
 	public HeartDisplay(double xPosition, double yPosition, int heartsToDisplay) {
-		this.containerXPosition = xPosition;
-		this.containerYPosition = yPosition;
+		this.container = new HBox();
+		this.container.setLayoutX(xPosition);
+		this.container.setLayoutY(yPosition);
 		this.numberOfHeartsToDisplay = heartsToDisplay;
-		initializeContainer();
 		initializeHearts();
 	}
-	
-	private void initializeContainer() {
-		container = new HBox();
-		container.setLayoutX(containerXPosition);
-		container.setLayoutY(containerYPosition);		
-	}
-	
+
 	private void initializeHearts() {
 		for (int i = 0; i < numberOfHeartsToDisplay; i++) {
 			ImageView heart = new ImageView(new Image(getClass().getResource(HEART_IMAGE_NAME).toExternalForm()));
