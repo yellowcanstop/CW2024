ef474ee7bccb569b6e9284b0ba2d63b19562afc9
yellowcanstop <yellowcanstop@gmail.com>
2024-11-04 01:46:07 +0800

    Refactor instantiateLevelView method to directly return instance of LevelViewLevelTwo.

--- src/main/java/com/example/demo/LevelTwo.java
+++ src/main/java/com/example/demo/LevelTwo.java
@@ -5,7 +5,6 @@ public class LevelTwo extends LevelParent {
 	private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
 	private static final int PLAYER_INITIAL_HEALTH = 5;
 	private final Boss boss;
-	private LevelViewLevelTwo levelView;
 
 	public LevelTwo(double screenHeight, double screenWidth) {
 		super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
@@ -36,8 +35,7 @@ public class LevelTwo extends LevelParent {
 
 	@Override
 	protected LevelView instantiateLevelView() {
-		levelView = new LevelViewLevelTwo(getRoot(), PLAYER_INITIAL_HEALTH);
-		return levelView;
+		return new LevelViewLevelTwo(getRoot(), PLAYER_INITIAL_HEALTH);
 	}
 
 }
