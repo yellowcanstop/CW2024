2d60e9f7efafa25bb4073b14c9fd04648132831d
yellowcanstop <yellowcanstop@gmail.com>
2024-11-04 00:55:52 +0800

    Add keyword this to instance methods in constructor to avoid ambiguity and improve clarity.

--- src/main/java/com/example/demo/GameOverImage.java
+++ src/main/java/com/example/demo/GameOverImage.java
@@ -8,10 +8,9 @@ public class GameOverImage extends ImageView {
 	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.png";
 
 	public GameOverImage(double xPosition, double yPosition) {
-		setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()) );
-//		setImage(ImageSetUp.getImageList().get(ImageSetUp.getGameOver()));
-		setLayoutX(xPosition);
-		setLayoutY(yPosition);
+		this.setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()) );
+		this.setLayoutX(xPosition);
+		this.setLayoutY(yPosition);
 	}
 
 }
