fadc4598cfecae2a2d42abf18b4d2b32886f3179
yellowcanstop <yellowcanstop@gmail.com>
2024-11-04 14:34:23 +0800

    Add AssetPaths to encapsulate the classpaths of all game assets for cleaner control and access.

---
+++ src/main/java/com/example/demo/assets/AssetPaths.java
@@ -0,0 +1,22 @@
+package com.example.demo.assets;
+
+/**
+ * Class to encapsulate the classpaths of all the assets used in the game for easy access.
+ */
+public class AssetPaths {
+
+    public static final String BACKGROUND1 = "/images/background1.jpg";
+    public static final String BACKGROUND2 = "/images/background2.jpg";
+    public static final String BOSS_PLANE = "/images/bossplane.png";
+    public static final String ENEMY_FIRE = "/images/enemyFire.png";
+    public static final String ENEMY_PLANE = "/images/enemyplane.png";
+    public static final String FIREBALL = "/images/fireball.png";
+    public static final String GAMEOVER = "/images/gameover.png";
+    public static final String HEART = "/images/heart.png";
+    public static final String SHIELD = "/images/shield.png";
+    public static final String USER_FIRE = "/images/userfire.png";
+    public static final String USER_PLANE = "/images/userplane.png";
+    public static final String YOU_WIN = "/images/youwin.png";
+
+    //public static final String GAMEOVER_SOUND = "/sounds/gameover.wav";
+}
