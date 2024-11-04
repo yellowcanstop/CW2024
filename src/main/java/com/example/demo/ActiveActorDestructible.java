606b1120bee1912f5b59c8555d32a467af5ca62a
yellowcanstop <yellowcanstop@gmail.com>
2024-11-04 23:59:19 +0800

    Remove redundant setDestroyed method because the boolean parameter is always set to true and hence can be included in the destroy method. Add javadocs.

--- src/main/java/com/example/demo/ActiveActorDestructible.java
+++ src/main/java/com/example/demo/ActiveActorDestructible.java
@@ -2,32 +2,57 @@ package com.example.demo;
 
 import com.example.demo.assets.*;
 
+/**
+ * Abstract class representing a sprite which can take damage and be destroyed in the game.
+ */
 public abstract class ActiveActorDestructible extends ActiveActor implements Destructible {
 
 	private boolean isDestroyed;
 
+	/**
+	 * Constructor to create an instance of ActiveActorDestructible.
+	 *
+	 * @param imageName - the String classpath of the image for the actor
+	 * @param imageHeight - the height of the image for the actor
+	 * @param initialXPos - the initial x coordinate position of the actor
+	 * @param initialYPos - the initial y coordinate position of the actor
+	 * @param imageManager - the ImageAssetManager to load the image for the actor
+	 */
 	public ActiveActorDestructible(String imageName, int imageHeight, double initialXPos, double initialYPos, ImageAssetManager imageManager) {
 		super(imageName, imageHeight, initialXPos, initialYPos, imageManager);
 		isDestroyed = false;
 	}
 
+	/**
+	 * Update the position of the actor.
+	 */
 	@Override
 	public abstract void updatePosition();
 
+	/**
+	 * Update the actor.
+	 */
 	public abstract void updateActor();
 
+	/**
+	 * Handle damage taken by the destructible.
+	 */
 	@Override
 	public abstract void takeDamage();
 
+	/**
+	 * Destroy the destructible.
+	 */
 	@Override
 	public void destroy() {
-		setDestroyed(true);
+		this.isDestroyed = true;
 	}
 
-	protected void setDestroyed(boolean isDestroyed) {
-		this.isDestroyed = isDestroyed;
-	}
-
+	/**
+	 * Check if the destructible is destroyed.
+	 *
+	 * @return true if destructible is destroyed, else false
+	 */
 	public boolean isDestroyed() {
 		return isDestroyed;
 	}
