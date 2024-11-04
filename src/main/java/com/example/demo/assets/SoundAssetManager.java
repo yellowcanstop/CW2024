31fce1f6b53929acae4a97793265b8f365e1faaf
yellowcanstop <yellowcanstop@gmail.com>
2024-11-04 14:32:04 +0800

    Add SoundAssetManager which extends AssetManager to track and explicitly unload sounds.

---
+++ src/main/java/com/example/demo/assets/SoundAssetManager.java
@@ -0,0 +1,59 @@
+package com.example.demo.assets;
+
+import com.example.demo.controller.AlertUtils;
+import javafx.scene.media.AudioClip;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// TODO - control volume? - AudioClip.setVolume(double)
+// TODO - use Media and MediaPlayer for longer audio clips - more memory-efficient
+
+/**
+ * Asset manager for {@code AudioClip} for sound effects in the game.
+ * <p>
+ * As per JavaFX docs, AudioClip stores in memory the raw, uncompressed audio data
+ * for the entire sound. This suits short clips which are played frequently.
+ * For background music, the AudioClip will loop continuously until stopped.
+ *
+ * @see <a href="https://docs.oracle.com/javafx/2/api/javafx/scene/media/AudioClip.html">doc for AudioClip</a>
+ */
+public class SoundAssetManager extends AssetManager<AudioClip> {
+
+    private Map<String, AudioClip> sounds = new HashMap<>();
+
+    /**
+     * Load an audio clip from the given path.
+     *
+     * @param path - String name of classpath
+     * @return the clip loaded from the path
+     */
+    @Override
+    public AudioClip load(String path) {
+        if (sounds.containsKey(path)) {
+            return sounds.get(path);
+        }
+        try {
+            AudioClip clip = new AudioClip(path);
+            sounds.put(path, clip);
+            return clip;
+        } catch (Exception e) {
+            AlertUtils.alertException(e);
+            return null;
+        }
+    }
+
+    /**
+     * Unload an audio clip from the given path.
+     *
+     * @param path - String name of classpath
+     */
+    @Override
+    public void unload(String path) {
+        try {
+            sounds.remove(path);
+        } catch (Exception e) {
+            AlertUtils.alertException(e);
+        }
+    }
+}
