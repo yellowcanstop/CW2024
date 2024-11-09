a027302041bb94411a1c7a9444929a171fd65b15
yellowcanstop <yellowcanstop@gmail.com>
2024-11-09 11:53:31 +0800

    Clean module exports.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,8 @@ module com.example.demo {
     exports com.example.demo.utils;
     opens com.example.demo.views to javafx.fxml;
     opens com.example.demo.levels to javafx.fxml;
-    opens com.example.demo.models to javafx.fxml;
     opens com.example.demo.views.components to javafx.fxml;
     exports com.example.demo;
+    exports com.example.demo.models;
+    opens com.example.demo.models to javafx.fxml;
 }
