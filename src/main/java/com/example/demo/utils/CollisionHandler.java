43f304e4a623e7b9f13457b23c8c308d87394c99
yellowcanstop <yellowcanstop@gmail.com>
2024-11-17 00:30:25 +0800

    Refactor CollisionHandler.

--- src/main/java/com/example/demo/utils/CollisionHandler.java
+++ src/main/java/com/example/demo/utils/CollisionHandler.java
@@ -3,50 +3,71 @@ package com.example.demo.utils;
 import com.example.demo.models.BossPlane;
 import com.example.demo.models.DestructibleSprite;
 import com.example.demo.models.UserBomb;
-import javafx.scene.media.AudioClip;
-
 import java.util.List;
-import java.util.Objects;
 
 public class CollisionHandler {
     public static final String DEACTIVATE_SHIELD = "/com/example/demo/sounds/minty_attack.wav";
-    private SoundLoader soundLoader = new SoundLoader(DEACTIVATE_SHIELD);
+    private final SoundLoader soundLoader = new SoundLoader(DEACTIVATE_SHIELD);
+
     /**
-     * Handle collisions between two lists of actors by checking if the bounds of any two actors intersect.
-     * <p>
-     * If the bounds of two actors intersect:
-     * <p>for projectiles, the projectile is destroyed;
-     * <p>for enemy planes, the enemy plane is destroyed;
-     * <p>for the user plane, it has its health decremented and is destroyed if its health reaches zero;
-     * <p>for the boss plane, it has its health decremented if unshielded and is destroyed if its health reaches zero;
-     * <p>for level three's bomb, it deactivates the boss plane's shield if it is active.
+     * Handle collisions between two lists of actors by checking if collision condition met for each pair of actors.
      *
-     * @param actors1 - the first list of actors
-     * @param actors2 - the second list of actors
+     * @param actors - the first list of actors
+     * @param otherActors - the second list of actors
      */
-    public void handleCollisions(List<DestructibleSprite> actors1,
-                                 List<DestructibleSprite> actors2) {
-        for (DestructibleSprite actor : actors2) {
-            for (DestructibleSprite otherActor : actors1) {
-                if (actor.getBoundsInParent().intersects(otherActor.getBoundsInParent())) {
-                    if (actor instanceof BossPlane && otherActor instanceof UserBomb) {
-                        boolean hit = ((BossPlane) actor).deactivateShield();
-                        if (hit) {
-                            soundLoader.playSound();
-                        }
-                        otherActor.takeDamage();
-                    } else if (actor instanceof UserBomb && otherActor instanceof BossPlane) {
-                        boolean hit = ((BossPlane) otherActor).deactivateShield();
-                        if (hit) {
-                            soundLoader.playSound();
-                        }
-                        actor.takeDamage();
-                    } else {
-                        actor.takeDamage();
-                        otherActor.takeDamage();
-                    }
+    public void handleCollisions(List<DestructibleSprite> actors, List<DestructibleSprite> otherActors) {
+        for (DestructibleSprite actor : actors) {
+            for (DestructibleSprite otherActor : otherActors) {
+                if (collided(actor, otherActor)) {
+                    handleCollision(actor, otherActor);
                 }
             }
         }
+    }
+
+    /**
+     * Check if collision has occurred by checking if the bounds of the two actors intersect.
+     *
+     * @param actor - the first actor
+     * @param otherActor - the second actor
+     * @return true if the actors have collided, else false
+     */
+    private boolean collided(DestructibleSprite actor, DestructibleSprite otherActor) {
+        return actor.getBoundsInParent().intersects(otherActor.getBoundsInParent());
+    }
+
+    /**
+     * Handle the collision between two actors by taking damage from each actor.
+     * <p>
+     * Special case for collision between boss and user bomb by calling handleBombCollisionWithBoss.
+     *
+     * @param actor - the first actor
+     * @param otherActor - the second actor
+     */
+    private void handleCollision(DestructibleSprite actor, DestructibleSprite otherActor) {
+        if (actor instanceof BossPlane && otherActor instanceof UserBomb) {
+            handleBombCollisionWithBoss((BossPlane) actor, (UserBomb) otherActor);
+            return;
+        }
+        if (actor instanceof UserBomb && otherActor instanceof BossPlane) {
+            handleBombCollisionWithBoss((BossPlane) otherActor, (UserBomb) actor);
+            return;
+        }
+        actor.takeDamage();
+        otherActor.takeDamage();
+    }
+
+    /**
+     * Handle the collision between boss and user bomb by deactivating the boss shield, playing a sound effect and cleaning the bomb.
+     *
+     * @param bossPlane - the boss plane
+     * @param userBomb - the bomb fired by the user
+     */
+    private void handleBombCollisionWithBoss(BossPlane bossPlane, UserBomb userBomb) {
+        boolean hit = bossPlane.deactivateShield();
+        if (hit) {
+            soundLoader.playSound();
+        }
+        userBomb.takeDamage();
     }
 }
