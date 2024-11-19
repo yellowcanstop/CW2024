2c2154f95eb9e9e273dbd3b4c9a5b66d0518d255
yellowcanstop <yellowcanstop@gmail.com>
2024-11-19 23:40:35 +0800

    Adjust look of user bullet.

--- src/main/java/com/example/demo/models/UserBullet.java
+++ src/main/java/com/example/demo/models/UserBullet.java
@@ -4,7 +4,7 @@ package com.example.demo.models;
  * Projectile for user-controlled player sprite in the game.
  */
 public class UserBullet extends Projectile {
-	private static final int IMAGE_HEIGHT = 125;
+	private static final int IMAGE_HEIGHT = 25;
 	private static final int HORIZONTAL_VELOCITY = 15;
 	public static final String USER_FIRE = "/com/example/demo/images/userfire.png";
 
